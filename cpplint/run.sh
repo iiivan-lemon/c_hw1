5cc1a2b4dfe8c7275c8f3c89eba611fe7fd2e537
iiivan-lemon <lipton2807@yandex.ru>
2021-11-04 21:17:57 +0300

    ci cpplint

--- cpplint/run.sh
+++ cpplint/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-python ./linters/cpplint/cpplint.py --extensions=c project/* 
-python ./linters/cpplint/cpplint.py --extensions=cpp tests/*
+python ./cpplint/cpplint.py --extensions=c project/* 
+python ./cpplint/cpplint.py --extensions=cpp tests/*
 
 
